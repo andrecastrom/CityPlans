98f969f0665a351609cd20280e18d5a98b484257
andrecastrom <andrecastromartinez@gmail.com>
2016-12-16 14:49:46 +0100

    empezando el modelo

---
+++ app/src/main/java/com/andrecastrom/cityplans/model/pojo/Event.java
@@ -0,0 +1,176 @@
+package com.andrecastrom.cityplans.model.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by 47369176d on 30/11/2016.
+ */
+
+public class Event {
+
+    private String id;
+    private String name;
+    private String description;
+    private String icon;
+    private String color;
+    private Date dateStart;
+    private Date dateEnd;
+    private String hourStart;
+    private String hourEnd;
+    private String price;
+    private String priceCurrency;
+    private String categoryName;
+    private String placeName;
+    private String placeLink;
+    private Float longitude;
+    private Float latitude;
+    private String web;
+    private String htmlFrame;
+
+
+    public Event () {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public Date getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(Date dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public String getHourStart() {
+        return hourStart;
+    }
+
+    public void setHourStart(String hourStart) {
+        this.hourStart = hourStart;
+    }
+
+    public String getHourEnd() {
+        return hourEnd;
+    }
+
+    public void setHourEnd(String hourEnd) {
+        this.hourEnd = hourEnd;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPriceCurrency() {
+        return priceCurrency;
+    }
+
+    public void setPriceCurrency(String priceCurrency) {
+        this.priceCurrency = priceCurrency;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPlaceLink() {
+        return placeLink;
+    }
+
+    public void setPlaceLink(String placeLink) {
+        this.placeLink = placeLink;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getHtmlFrame() {
+        return htmlFrame;
+    }
+
+    public void setHtmlFrame(String htmlFrame) {
+        this.htmlFrame = htmlFrame;
+    }
+}
---
+++ app/src/main/java/com/andrecastrom/cityplans/presenter/IDataMapPresenter.java
@@ -0,0 +1,8 @@
+package com.andrecastrom.cityplans.presenter;
+
+/**
+ * Created by 47369176d on 16/12/2016.
+ */
+
+public interface IDataMapPresenter {
+}
